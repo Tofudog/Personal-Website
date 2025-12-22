7449270ffb4971c7b223cf2aaf5dc363cceb1db6
Tofudog <leodefarias25@gmail.com>
2025-12-22 15:02:10 -0600

    Add section items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,34 @@
 
 const Navbar = () => {
-    return <>
-        <div>
-            <h1></h1>
+  return (
+    <div className="w-full py-4">
+      <div className="absolute left-10">
+        <h1 className="text-xl font-bold">לאו_</h1>
+      </div>
+
+      <div className="max-w-6xl mx-auto flex justify-center">
+        <div className="hidden lg:flex gap-10">
+          <div className="navbar-section">
+            <p>001</p>
+            <a>// home</a>
+          </div>
+          <div className="navbar-section">
+            <p>010</p>
+            <a>// experiences</a>
+          </div>
+          <div className="navbar-section">
+            <p>011</p>
+            <a>// projects</a>
+          </div>
+          <div className="navbar-section">
+            <p>100</p>
+            <a>// contact</a>
+          </div>
         </div>
-    </>
+      </div>
+    </div>
+  );
 };
+
 
 export default Navbar;
