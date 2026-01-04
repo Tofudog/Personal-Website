9b805e6e173dd18a81081debdedbf0921f466084
Tofudog <leodefarias25@gmail.com>
2026-01-04 15:45:45 -0300

    Fix contact logo outline

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -2,8 +2,15 @@
 const Bottom = () => {
     return <>
         <div className="full-width">
-            <div class="flex relative bg-[#1f1d1a] w-full h-75 justify-center items-center">
-                <h4 className="copyright">Copyright © 2026 Leonardo Amato Regis de Farias</h4>
+            <div className="flex flex-col relative bg-[#1f1d1a] w-full h-75 justify-center items-center">
+                <h4 className="copyright">
+                    Copyright © 2026 Leonardo Amato Regis de Farias
+                </h4>
+                <div className="flex gap-4 mt-4">
+                    <div className="circle">logo1</div>
+                    <div className="circle">logo2</div>
+                    <div className="circle">logo3</div>
+                </div>
             </div>
         </div>
     </>
