8b6c553c0d29d5be1b9c63b3071d8a9b502b6f23
Tofudog <leodefarias25@gmail.com>
2026-09-18 11:02:32 -0400

    Include white top border on the bottom

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -6,7 +6,7 @@ import codeforces_logo from "./../assets/codeforces.png";
 const Bottom = () => {
     return (
         <footer className="relative z-10 w-full">
-            <div className="box-border flex flex-col relative bg-[#2b2824] w-full h-75 justify-center items-center pb-[env(safe-area-inset-bottom,0px)]">
+            <div className="border-t-1 flex flex-col relative bg-[#2b2824] w-full h-75 justify-center items-center pb-[env(safe-area-inset-bottom,0px)]">
                 <h4 className="copyright">
                     Copyright © 2026 Leonardo Amato Regis de Farias
                 </h4>
